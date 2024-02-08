2174984b3992f8268e74aed5ad29b79dba63c71b
sl4y3r <99611700+SL4Y3RO4@users.noreply.github.com>
2024-02-08 21:42:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,19 @@ let win;
 let x = (Math.random() * 500) + 1;
 let y = (Math.random() * 500) + 1;
 let delay;
+let img;
 
 //body
 
+/*
+img = document.createElement('img');
+img.src = 'oof.jpg';
+*/
 window.addEventListener('load', () => { 
 
 delay = setInterval(function()  {
 
-win = window.open('','', 'height=' + h + ',' + 'width=' + w + ',' + 'left=' + x + 'px' + ',' + 'top=' + y + 'px');
-win.document.write('Game Over');
+win = window.open('https://png.pngitem.com/pimgs/s/418-4181737_roblox-oof-hd-png-download.png','', 'height=' + h + ',' + 'width=' + w + ',' + 'left=' + x + 'px' + ',' + 'top=' + y + 'px');
 window.location.reload();
 win.document.body.style.color = "red";
 
